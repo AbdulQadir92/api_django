aaab91eddc1d65a98db1276d66fb034da9df0de6
AbdulQadir92 <a_qadir_jamali@hotmail.com>
2022-09-19 18:12:12 +0500

    Allowed all hosts on cors

--- api_django/settings.py
+++ api_django/settings.py
@@ -126,11 +126,13 @@ USE_L10N = True
 USE_TZ = True
 
 
-CORS_ALLOWED_ORIGINS = [
-    'https://basic-api-react.netlify.app',
-    "http://localhost:3000",
-    "http://127.0.0.1:3000"
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'https://basic-api-react.netlify.app',
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000"
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
